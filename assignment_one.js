a1a736f35be3cea34b0bb2394dfcdf33b38c6111
SAISUKHITHKODALI <kodalisukhith@gmail.com>
2022-05-13 01:24:59 -0400

    frst assignment

---
+++ assignment_one.js
@@ -0,0 +1,25 @@
+let form = document.getElementById("form-main");
+
+let table = ducument.getElementById("table");
+form.addEventListener("SUBMIT",(e)=>{
+    e.preventDefault();
+    SVGNumberList();
+})
+
+const SUBMIT=()=>{
+  let firstname = document.getElementById("firstname").nodeValue;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
